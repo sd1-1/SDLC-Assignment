ff12dfa36b9a23797f082692705269a57f011bab
sd1-1 <darrsuleman@gmail.com>
2023-02-17 00:03:51 +0000

    created user class in models folder

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
-from flask import Flask, render_template, redirect, request, session, jsonify
+from flask import Flask, render_template, redirect, request
 from pymongo import MongoClient
 import json
 from bson.json_util import dumps
 from hashlib import sha256
 import numpy as np
+from user.models import User
 
 client = MongoClient(
     "mongodb+srv://sdlcadmin:Apples123@cluster0.euazjbc.mongodb.net/?retryWrites=true&w=majority")
@@ -15,18 +16,17 @@ app.secret_key = "abc123"  # required for session and used to encrpyt the cookie
 
 @app.route('/')
 def index():
-    # check whether user is already logged in using session
-    if 'loggedIn' in session and session['loggedIn']:
+    if User.loggedin():  # check whether user is already logged in using session
         return render_template('landing.html')
     else:
+
         # go back to the login page if they are not logged in
         return render_template('index.html')
 
 
 @app.route('/landing')
 def landing():
-
-    if 'loggedIn' in session and session['loggedIn']:
+    if User.loggedin():
         return render_template('landing.html')
     else:
         return redirect("/")
@@ -34,7 +34,7 @@ def landing():
 
 @app.route('/quizresults', methods=['POST', 'GET'])
 def quizresults():
-    if 'loggedIn' in session and session['loggedIn']:
+    if User.loggedin():
 
         # get the form data that has been submitted
         module = request.form.get("module")
@@ -65,28 +65,15 @@ def quizresults():
 
 @app.route('/login', methods=['POST'])
 def login():
-    matching_record = db.users_records.find_one({
-        "email": request.form.get("useremail")
-    })
-
-    # hashing the password so it can be compared with the hash in the database
-    password_hash = sha256(request.form.get(
-        "userpassword").encode('utf-8')).hexdigest()
-
-    if matching_record and password_hash == matching_record["password"]:
-
-        session['loggedIn'] = True
-        return redirect("landing")
-    return redirect("/")
+    return User.login()
 
 
 @app.route('/viewdata')
 def viewdata():
-    if 'loggedIn' in session and session['loggedIn']:
+    if User.loggedin():
         cursor = db.sdlc_records.find()  # gets all data in the sdlc_records collection
         list_cursor = list(cursor)
         json_data = dumps(list_cursor)
-
         return json_data
     else:
         return redirect("/")
@@ -94,8 +81,7 @@ def viewdata():
 
 @app.route('/uploadnewdata', methods=['POST'])
 def uploadnewdata():
-
-    if 'loggedIn' in session and session['loggedIn']:
+    if User.loggedin():
         data = request.files['file']
         info = json.loads(data.read())
         # delete everything before looping through each document and inserting it into the database
@@ -105,32 +91,26 @@ def uploadnewdata():
                 student_data
             )
         return "Success"
-
     else:
         return redirect("/")
 
 
 @app.route('/signout')
 def signout():
-    # change session status to false when the signout button is clicked
-    session["loggedIn"] = False
-    return redirect("/")
+    return User.signout()
 
 
 @app.route('/updateexistingdata', methods=['POST'])
 def updateexistingdata():
-    if 'loggedIn' in session and session['loggedIn']:
+    if User.loggedin():
 
         data = request.files['files2']
-
         info = json.loads(data.read())
 
         # This enables us to update the matching document with the same student_id as inputted in the json
         filter = {'student_id': info['student_id']}
-
         newvalues = info
         db.sdlc_records.replace_one(filter, newvalues)
-
         return "Successfully updated existing data, please press back"
     else:
         return redirect("/")
@@ -138,20 +118,15 @@ def updateexistingdata():
 
 @app.route('/deletedata', methods=['POST'])
 def deletedata():
-    if 'loggedIn' in session and session['loggedIn']:
+    if User.loggedin():
 
         data = request.files['files3']
-
         info = json.loads(data.read())
-
         filter = {'student_id': info['student_id']}
-
         db.sdlc_records.delete_one(filter)
-
         return "Successfully deleted data, please press back"
     else:
         return redirect("/")
-
 
 if __name__ == "__main__":
     app.run(debug=True)
---
+++ user/models.py
@@ -0,0 +1,36 @@
+from flask import Flask, jsonify, request, session, redirect
+from hashlib import sha256
+from pymongo import MongoClient
+client = MongoClient(
+    "mongodb+srv://sdlcadmin:Apples123@cluster0.euazjbc.mongodb.net/?retryWrites=true&w=majority")
+db = client.get_database("sdlc_db")  # connection to database as the admin
+
+
+
+
+class User():
+
+    def login():
+        matching_record = db.users_records.find_one({
+            "email": request.form.get("useremail")
+        })
+
+        # hashing the password so it can be compared with the hash in the database
+        password_hash = sha256(request.form.get(
+            "userpassword").encode('utf-8')).hexdigest()
+
+        if matching_record and password_hash == matching_record["password"]:
+
+            session['loggedIn'] = True
+            return redirect("landing")
+        return redirect("/")
+
+    def signout():
+        # change session status to false when the signout button is clicked
+        session["loggedIn"] = False
+        return redirect("/")
+
+    def loggedin():
+        if 'loggedIn' in session and session['loggedIn']:
+            return True
+        
