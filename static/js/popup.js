ca80ea03a1543be63fdab287766d06fc04f41f3d
sd1-1 <darrsuleman@gmail.com>
2023-02-23 22:23:28 +0000

    added js to only allow for one form at the same time

--- static/js/popup.js
+++ static/js/popup.js
@@ -1,7 +1,15 @@
 
 
+function closeAllForms(){
+
+  document.getElementById("uploadForm").style.display = "none";
+  document.getElementById("updateForm").style.display = "none";
+  document.getElementById("deleteForm").style.display = "none";
+}
+
+
 function openForm(formName) {
-    
+    closeAllForms()
     document.getElementById(formName).style.display = "flex";
 }
   
